1c2851330051a364e71e5f54231c22de8ebf8fc0
Mart <mart.willocx@hotmail.com>
2018-11-10 21:07:53 +0100

    Added Babylon and fixed once more the link selection process

--- filters/reddit-to-deathworlders.js
+++ filters/reddit-to-deathworlders.js
@@ -10,6 +10,9 @@ function apply(params, next)
     const $ = params.chap.dom;
 
     var out_links = $("a:contains('NOW CLICK HERE TO READ')");
+    if(out_links.length<1){
+        out_links = $("a:contains('LINK')");
+    }
     if(out_links.length>0){
         // We should follow the link.
         params.chap.src = out_links[0].attribs["href"];
